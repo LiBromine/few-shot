78a9ea71c863c34987dd64503baa3cd1c07c820a
LiBr <zplibingrui@163.com>
2021-06-13 18:34:11 +0800

    complete the args help

--- common/args.py
+++ common/args.py
@@ -10,9 +10,9 @@ def parse_args():
     parser.add_argument('--learning_rate', type=float, default=1e-3,
         help='Learning rate during optimization. Default: 1e-3')
     parser.add_argument('--weight_decay', type=float, default=5e-4,
-        help='TODO')
+        help='Weight decay during optimization')
     parser.add_argument('--momentum', type=float, default=0.9,
-        help='TODO')
+        help='Momentum during optimization')
     parser.add_argument('--drop_rate', type=float, default=0.5,
         help='Drop rate of the Dropout Layer. Default: 0.5')
     parser.add_argument('--is_train', type=int, default=1,
@@ -24,30 +24,30 @@ def parse_args():
     parser.add_argument('--inference_version', type=int, default=0,
         help='The version for inference. Set 0 to use latest checkpoint. Default: 0')
     parser.add_argument('--continue_train', type=int, default=0,
-        help='TODO')
+        help='Do or not continue to train')
 
     parser.add_argument('--p_mean', type=float, default=0.9,
-        help='TODO')
+        help='The p_mean parameter in ActivationNet')
     parser.add_argument('--val_num_shots', type=int, default=8,
-        help='TODO')
+        help='Number of shots used in validation')
     parser.add_argument('--seed', type=int, default=0,
-        help='TODO')
+        help='Random seed')
     parser.add_argument('--num_units', type=int, default=4096,
-        help='TODO')
+        help='Hidden layer units')
     parser.add_argument('--num_shots', type=int, default=10,
-        help='TODO')
+        help='Number of shots used in test/inference')
     parser.add_argument('--pretrained', type=bool, default=True,
-        help='TODO')
+        help='Do or not use pretrained AlexNet')
     parser.add_argument('--large_data_dir', type=str, default='../../Proj2',
-        help='TODO')
+        help='Training data directory')
     parser.add_argument('--large_data_name', type=str, default='base_feature.npy',
-        help='TODO')
+        help='Training data file')
     parser.add_argument('--few_data_dir', type=str, default='../../Proj2/training',
-        help='TODO')
+        help='few shot data directory')
     parser.add_argument('--test_data_dir', type=str, default='../../Proj2/test',
-        help='TODO')
+        help='Test data directory')
     parser.add_argument('--output', type=str, default='./output.txt',
-        help='TODO')
+        help='Output file path')
 
     # Checkpointing
     # parser.add_argument("--exp-name", type=str, default=None, help="name of the experiment")
